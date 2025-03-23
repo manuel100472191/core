abc7d74b558505b74550f4b661af48ddf5a4ec88
gabrielo <gabrielorteganicolas@gmail.com>
2025-03-23 11:53:09 +0100

    deleted

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -52,6 +52,7 @@ public:
     struct GetProjectNumbers_output
     {
         uint64 numberOfProjects;
+        uint64 title;
     };
 
 private:
@@ -136,6 +137,7 @@ private:
 
     PUBLIC_FUNCTION(GetProjectNumbers)
         output.numberOfProjects = state.numberOfProjects;
+        output.name = state.mProjectName.get(0);
     _
 
     PUBLIC_FUNCTION(GetStats)
