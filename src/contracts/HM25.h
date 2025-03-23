09e9fe178a5e5df393de831f9068b11c37898eaf
gabrielo <gabrielorteganicolas@gmail.com>
2025-03-23 03:54:02 +0100

    Implemented titles and investments creation

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -24,17 +24,37 @@ public:
     struct Burn_input{};
     struct Burn_output{};
 
-    struct CreateProject_input {};
+    struct CreateProject_input {
+        uint64 title;
+    };
     struct CreateProject_output {
         uint64 projectIndex;
     };
 
     struct CreateMilestone_input {
         uint64 projectIndex;
+        uint64 title;
     };
     struct CreateMilestone_output {
         uint64 milestoneIndex;
     };
+
+    struct CreateMilestone_output {
+        uint64 currentMilestoneIndex;
+    }
+
+    struct CreateInvestment_input {
+        uint64 projectIndex;
+        uint64 milestoneIndex;
+        uint64 amount;
+    };
+    struct CreateInvestment_output {
+        uint64 investmentIndex;
+    };
+
+    struct CreateInvestment_locals {
+        uint64 investmentIndex;
+    }
 
     struct GetProjects_input {};
     struct GetProjects_output
@@ -64,20 +84,23 @@ private:
 
     // Project related arrays
     Array<id, MAX_PROJECTS> mProjectCreator;
-    // TODO project name and description
+    // Project name 
+    Array<uint64, MAX_PROJECTS> mProjectName;
     Array<bit, MAX_PROJECTS> mProjectActive;
     // Project ->  milestones -> { id -> amounts }
 
     // Milestones related arrays
-    Array<id, MAX_PROJECTS*MAX_PROJECT_MILESTONES> mMilestoneCreator;
+    Array<uint64, MAX_PROJECTS*MAX_PROJECT_MILESTONES> mMilestoneTitle;
     Array<id, MAX_PROJECTS> mNumberOfMilestonesPerProject;
-
-    Array<id, MAX_PROJECTS*MAX_PROJECT_MILESTONES*MAX_INVESTORS_PER_MILESTONE> mMilestoneInvestor; // Saves the investor id 
-    Array<id, MAX_PROJECTS*MAX_PROJECT_MILESTONES*MAX_INVESTORS_PER_MILESTONE> mMilestoneAmountPerInvestor; // Amount related to the investor 
-
+    
     Array<uint32, MAX_PROJECTS*MAX_PROJECT_MILESTONES> mMilestoneDueDate; // TODO see date format and make it automatic (?)
     Array<bit, MAX_PROJECTS*MAX_PROJECT_MILESTONES> mMilestoneAchieved;
     // TODO create milestone name and description arrays
+    
+    // Investment related arrays
+    Array<id, MAX_PROJECTS*MAX_PROJECT_MILESTONES*MAX_INVESTORS_PER_MILESTONE> mMilestoneInvestor; // Saves the investor id 
+    Array<id, MAX_PROJECTS*MAX_PROJECT_MILESTONES*MAX_INVESTORS_PER_MILESTONE> mMilestoneAmountPerInvestor; // Amount related to the investor 
+    Array<uint64, MAX_PROJECTS*MAX_PROJECT_MILESTONES> mNumberOfInvestorsPerMilestone;
 
     /**
      * @output projectIndex
@@ -95,6 +118,10 @@ private:
         }
         // Save the creator
         state.mProjectCreator.set(state.numberOfProjects, qpi.invocator());
+        // Save the title
+        state.mProjectName.set(state.numberOfProjects, input.title);
+        // Set the project as active
+        state.mProjectActive.set(state.numberOfProjects, 1);
         output.projectIndex = state.numberOfProjects;
         state.numberOfProjects++;
         // Set its number of milestones to 0
@@ -116,9 +143,31 @@ private:
             return;
         }
         // Save the creator
-        state.mMilestoneCreator.set(input.projectIndex*MAX_PROJECT_MILESTONES + state.mNumberOfMilestonesPerProject.get(input.projectIndex), qpi.invocator());
+        locals.currentMilestoneIndex = input.projectIndex*MAX_PROJECT_MILESTONES + state.mNumberOfMilestonesPerProject.get(input.projectIndex);
+        state.mMilestoneTitle.set(locals.currentMilestoneIndex, input.title);
         output.milestoneIndex = state.mNumberOfMilestonesPerProject.get(input.projectIndex);
         state.mNumberOfMilestonesPerProject.set(input.projectIndex, state.mNumberOfMilestonesPerProject.get(input.projectIndex) + 1);
+        // Set the number of investors of the created milestone to 0
+        state.mNumberOfInvestorsPerMilestone.set(locals.currentMilestoneIndex, 0);
+    _
+    
+    /**
+     * @output investmentIndex
+     */
+    PUBLIC_PROCEDURE(CreateInvestment)
+        if (qpi.invocator() != qpi.originator())
+        {
+            // return any leftover
+            qpi.transfer(qpi.invocator(), qpi.invocationReward());
+            return;
+        }
+
+        // Save the investor
+        locals.investmentIndex = input.projectIndex*MAX_PROJECT_MILESTONES*MAX_INVESTORS_PER_MILESTONE + input.milestoneIndex*MAX_INVESTORS_PER_MILESTONE;
+        state.mMilestoneInvestor.set(locals.investmentIndex, qpi.invocator());
+        state.mMilestoneAmountPerInvestor.set(locals.investmentIndex, input.amount);
+        state.mNumberOfInvestorsPerMilestone.set(input.projectIndex*MAX_PROJECT_MILESTONES + input.milestoneIndex, state.mNumberOfInvestorsPerMilestone.get(input.projectIndex*MAX_PROJECT_MILESTONES + input.milestoneIndex) + 1);
+        output.investmentIndex = locals.investmentIndex;
     _
 
     /**
@@ -163,6 +212,8 @@ private:
         REGISTER_USER_PROCEDURE(Echo, 1);
         REGISTER_USER_PROCEDURE(Burn, 2);
         REGISTER_USER_PROCEDURE(CreateProject, 3);
+        REGISTER_USER_PROCEDURE(CreateMilestone, 4);
+        REGISTER_USER_PROCEDURE(CreateInvestment, 5);
 
         REGISTER_USER_FUNCTION(GetStats, 1);
         REGISTER_USER_FUNCTION(GetProjects, 2);
